83569a887736031d531c6ef57503f550c3c698e5
Marcos Cannabrava <mpcannabrava@gmail.com>
2019-11-21 07:06:30 -0300

    changed root route and updated readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Article.new(title: Faker::TvShows::RickAndMorty.character,
+    content: Faker::TvShows::RickAndMorty.quote).save
+end
